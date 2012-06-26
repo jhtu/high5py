4c5ebb4c4af7df08a6bf9e16bacfc5d4e1f8600b
Jonathan Tu (Yellowstone) <jhtu@princeton.edu>
2012-06-25 21:11:23 -0400

    Tests can now be run from the h5io namespace.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 # Setup top level namespace
 from h5io import load_dataset, rename_dataset, save_array, append_scalars
-
+from alltests import run_all_tests
+import testh5io
---
+++ alltests.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import unittest
+
+import helper
+
+
+# Discover and run tests
+def run_all_tests():
+    test_loader = unittest.defaultTestLoader
+    test_suites = test_loader.discover(os.path.dirname(__file__))
+    unittest.TextTestRunner(buffer=True).run(test_suites)
+
+
+# Main routine
+if __name__ == '__main__':
+    helper.add_to_path('src')
+    run()
