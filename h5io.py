0b0353f6e7a96df0db0967fdb11a768ec4780dfc
Jonathan Tu (work) <Jonathan Tu (work)@localhost>
2017-08-11 08:27:45 -0700

    When saving arrays, can now choose to truncate file or not.

--- h5io.py
+++ h5io.py
@@ -1,21 +1,21 @@
 from collections import Iterable
 
-import numpy as np      
-import h5py 
+import numpy as np
+import h5py
 
 
 # Check that a dataset exists
 def exists(path_to_file, path_to_check_for):
-    fid = h5py.File(path_to_file, 'r') 
+    fid = h5py.File(path_to_file, 'r')
     available_paths = []
     fid.visit(available_paths.append)
     fid.close()
     return path_to_check_for in available_paths
 
 
-# Load a dataset 
+# Load a dataset
 def load_dataset(path_to_file, path_to_dataset):
-    fid = h5py.File(path_to_file, 'r') 
+    fid = h5py.File(path_to_file, 'r')
     path_to_dataset = unicode(path_to_dataset)
     data = fid[path_to_dataset][...]
     fid.close()
@@ -23,8 +23,8 @@ def load_dataset(path_to_file, path_to_dataset):
 
 
 # Rename a dataset
-def rename_dataset(path_to_file, old_path_to_dataset, new_path_to_dataset, 
-    new_desc=None):
+def rename_dataset(
+    path_to_file, old_path_to_dataset, new_path_to_dataset, new_desc=None):
     old_path_to_dataset = unicode(old_path_to_dataset)
     new_path_to_dataset = unicode(new_path_to_dataset)
     fid = h5py.File(path_to_file, 'a')
@@ -37,15 +37,19 @@ def rename_dataset(path_to_file, old_path_to_dataset, new_path_to_dataset,
 
 # Save an array as a dataset.  If array is complex-valued, a group will be
 # created containing the real and imaginary parts as separate datasets.
-def save_array(path_to_file, array, name, desc):
+def save_array(path_to_file, array, name, desc, truncate=False):
+    if truncate:
+        file_mode = 'w'
+    else:
+        file_mode = 'a'
     name = unicode(name)
-    fid = h5py.File(path_to_file, 'w') 
+    fid = h5py.File(path_to_file, file_mode)
     if np.iscomplexobj(array):
         fid.create_group(name)
         fid[name].create_dataset('real', data=array.real)
         fid[name + '/real'].attrs['Description'] = 'Real part'
         fid[name].create_dataset('imag', data=array.imag)
-        fid[name + '/imag'].attrs['Description'] = 'Imaginary part' 
+        fid[name + '/imag'].attrs['Description'] = 'Imaginary part'
     else:
         fid.create_dataset(name, data=array)
     fid[name].attrs['Description'] = desc
@@ -60,10 +64,10 @@ def append_scalars(path_to_file, scalars, names, descs):
         descs = [descs]
 
     if not len(scalars) == len(names) == len(descs):
-        raise ValueError('Mismatch in number of scalars, names, and/or '+\
-            'descs.')
+        raise ValueError(
+            'Mismatch in number of scalars, names, and/or descs.')
 
-    fid = h5py.File(path_to_file, 'a') 
+    fid = h5py.File(path_to_file, 'a')
     for s, n, d in zip(scalars, names, descs):
         n = unicode(n)
         if np.iscomplexobj(s):
@@ -76,9 +80,3 @@ def append_scalars(path_to_file, scalars, names, descs):
             fid.create_dataset(n, data=s)
         fid[n].attrs['Description'] = d
     fid.close()
-
-
-
-
-
-
