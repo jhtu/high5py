96ba9efc471cdd832021c1f2be218e29d3b76aed
Jonathan Tu (work) <none@none>
2017-08-11 08:40:32 -0700

    New method for appending arrays.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # Setup top level namespace
 from h5io import exists, load_dataset, rename_dataset, save_array,\
-    append_scalars
+    append_array, append_scalars
 from alltests import run_all_tests
 import testh5io
