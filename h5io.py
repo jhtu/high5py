4038e60d213cbf790407e82e64a71b5416b75d79
Jonathan Tu <none@none>
2020-04-14 08:16:03 -0700

    Open files using "with" syntax.

--- h5io.py
+++ h5io.py
@@ -6,19 +6,17 @@ import h5py
 
 # Check that a dataset exists
 def exists(path_to_file, path_to_check_for):
-    fid = h5py.File(path_to_file, 'r')
     available_paths = []
-    fid.visit(available_paths.append)
-    fid.close()
+    with h5py.File(path_to_file, 'r') as fid:
+        fid.visit(available_paths.append)
     return path_to_check_for in available_paths
 
 
 # Load a dataset
 def load_dataset(path_to_file, path_to_dataset):
-    fid = h5py.File(path_to_file, 'r')
     path_to_dataset = u'{}'.format(path_to_dataset)
-    data = fid[path_to_dataset][...]
-    fid.close()
+    with h5py.File(path_to_file, 'r') as fid:
+        data = fid[path_to_dataset][...]
     if len(data.shape) == 0:
         return np.asscalar(data)
     return data
@@ -29,12 +27,11 @@ def rename_dataset(
     path_to_file, old_path_to_dataset, new_path_to_dataset, new_desc=None):
     old_path_to_dataset = u'{}'.format(old_path_to_dataset)
     new_path_to_dataset = u'{}'.format(new_path_to_dataset)
-    fid = h5py.File(path_to_file, 'a')
-    fid[new_path_to_dataset] = fid[old_path_to_dataset]
-    if new_desc is not None:
-        fid[new_path_to_dataset].attrs['Description'] = new_desc
-    del fid[old_path_to_dataset]
-    fid.close()
+    with h5py.File(path_to_file, 'a') as fid:
+        fid[new_path_to_dataset] = fid[old_path_to_dataset]
+        if new_desc is not None:
+            fid[new_path_to_dataset].attrs['Description'] = new_desc
+        del fid[old_path_to_dataset]
 
 
 # Save an array as a dataset.  If array is complex-valued, a group will be
@@ -45,10 +42,9 @@ def save_array(path_to_file, array, name, desc, truncate=True):
     else:
         file_mode = 'a'
     name = u'{}'.format(name)
-    fid = h5py.File(path_to_file, file_mode)
-    fid.create_dataset(name, data=array)
-    fid[name].attrs['Description'] = desc
-    fid.close()
+    with h5py.File(path_to_file, file_mode) as fid:
+        fid.create_dataset(name, data=array)
+        fid[name].attrs['Description'] = desc
 
 
 # Append an array as a dataset.
@@ -67,9 +63,8 @@ def append_scalars(path_to_file, scalars, names, descs):
         raise ValueError(
             'Mismatch in number of scalars, names, and/or descs.')
 
-    fid = h5py.File(path_to_file, 'a')
-    for s, n, d in zip(scalars, names, descs):
-        n = u'{}'.format(n)
-        fid.create_dataset(n, data=s)
-        fid[n].attrs['Description'] = d
-    fid.close()
+    with h5py.File(path_to_file, 'a') as fid:
+        for s, n, d in zip(scalars, names, descs):
+            n = u'{}'.format(n)
+            fid.create_dataset(n, data=s)
+            fid[n].attrs['Description'] = d
