b41f982051bb274a6d020cf1f85f9ffcfa770e30
Jonathan Tu <none@none>
2020-04-14 09:30:59 -0700

    Changed variable names.

--- h5io.py
+++ h5io.py
@@ -1,49 +1,47 @@
-from collections import Iterable
-
 import numpy as np
 import h5py
 
 
 # Check that a dataset exists
-def exists(path_to_file, path_to_check_for):
-    available_paths = []
-    with h5py.File(path_to_file, 'r') as fid:
-        fid.visit(available_paths.append)
-    return path_to_check_for in available_paths
+def exists(file_path, target_dataset_path):
+    avail_dataset_paths = []
+    with h5py.File(file_path, 'r') as fid:
+        fid.visit(avail_dataset_paths.append)
+    return target_dataset_path in avail_dataset_paths
 
 
 # Load a dataset
-def load_dataset(path_to_file, path_to_dataset):
-    path_to_dataset = u'{}'.format(path_to_dataset)
-    with h5py.File(path_to_file, 'r') as fid:
-        data = fid[path_to_dataset][()]
+def load_dataset(file_path, dataset_path):
+    dataset_path = u'{}'.format(dataset_path)
+    with h5py.File(file_path, 'r') as fid:
+        data = fid[dataset_path][()]
     return data
 
 
 # Rename a dataset
 def rename_dataset(
-    path_to_file, old_path_to_dataset, new_path_to_dataset, new_desc=None):
-    old_path_to_dataset = u'{}'.format(old_path_to_dataset)
-    new_path_to_dataset = u'{}'.format(new_path_to_dataset)
-    with h5py.File(path_to_file, 'a') as fid:
-        fid[new_path_to_dataset] = fid[old_path_to_dataset]
-        if new_desc is not None:
-            fid[new_path_to_dataset].attrs['Description'] = new_desc
-        del fid[old_path_to_dataset]
+    file_path, old_dataset_path, new_dataset_path, new_description=None):
+    old_dataset_path = u'{}'.format(old_dataset_path)
+    new_dataset_path = u'{}'.format(new_dataset_path)
+    with h5py.File(file_path, 'a') as fid:
+        fid[new_dataset_path] = fid[old_dataset_path]
+        if new_description is not None:
+            fid[new_dataset_path].attrs['Description'] = new_description
+        del fid[old_dataset_path]
 
 
 # Save a dataset
-def save_dataset(path_to_file, data, name, desc, truncate=True):
+def save_dataset(file_path, data, dataset_path, description, truncate=True):
     if truncate:
         file_mode = 'w'
     else:
         file_mode = 'a'
-    name = u'{}'.format(name)
-    with h5py.File(path_to_file, file_mode) as fid:
-        fid.create_dataset(name, data=array)
-        fid[name].attrs['Description'] = desc
+    dataset_path = u'{}'.format(dataset_path)
+    with h5py.File(file_path, file_mode) as fid:
+        fid.create_dataset(dataset_path, data=data)
+        fid[dataset_path].attrs['Description'] = description
 
 
 # Append a dataset
-def append_dataset(path_to_file, data, name, desc):
-    save_array(path_to_file, array, name, desc, truncate=False)
+def append_dataset(file_path, data, dataset_path, description):
+    save_array(file_path, data, dataset_path, description, truncate=False)
