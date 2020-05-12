6fd69a13e21a3d3e1d4617ccb78a911a396bc14d
Jonathan Tu <none@none>
2020-05-12 11:13:08 -0700

    Changed docs style.

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = ['sphinx.ext.autodoc',
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 napoleon_use_param = True
+napoleon_use_rtype = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
