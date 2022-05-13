2824ad4bafab7fb64b9b431a7828fec3c096279f
Jonathan Tu <none@none>
2022-05-13 08:49:30 -0700

    Simplied setup file.  pip install works.

--- setup.py
+++ setup.py
@@ -1,47 +1,29 @@
 import os
-from setuptools import setup, find_packages
-from pkg_resources import parse_version
+from setuptools import setup
+import sys
 
 
-# Locate current location
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Get the version from the relevant file
-with open(os.path.join(os.path.join(here, 'high5py'), '_version.py')) as f:
-    exec(f.read())
-
-# Get the development status from the version string
-parsed_version = str(parse_version(__version__))
-if any(w in ['*a', '*alpha'] for w in parsed_version):
-    devstatus = 'Development Status :: 3 - Alpha'
-elif any(w in ['*b', '*beta'] for w in parsed_version):
-    devstatus = 'Development Status :: 4 - Beta'
-else:
-    devstatus = 'Development Status :: 5 - Production/Stable'
+# Locate version module and import
+pkg_name = 'high5py'
+curr_dir = os.path.abspath(os.path.dirname(__file__))
+version_dir = os.path.join(curr_dir, pkg_name)
+sys.path.append(version_dir)
+from _version import __version__
 
 # Setup
 setup(
     name='high5py',
     version=__version__,
-    description='Interact with HDF5 files using one-line function calls.',
-    author='Jonathan Tu',
+    description='Interact with HDF5 files using one-line function calls',
+    author='Jonathan H. Tu',
     url='http://high5py.readthedocs.io',
-    maintainer='Jonathan Tu',
+    maintainer='Jonathan H. Tu',
     license='Free BSD',
     classifiers=[
-        # How mature is this project? Common values are
-        # 3 - Alpha
-        # 4 - Beta
-        # 5 - Production/Stable
-        devstatus,
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3'
     ],
-    packages=['high5py'],
-    package_dir={'high5py': 'high5py'},
+    packages=[pkg_name],
     install_requires=['h5py', 'numpy']
 )
